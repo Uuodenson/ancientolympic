707305d895bd6ef225860ae90be9f7177f3f2065
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2024-08-25 09:06:10 +0200

    start

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="ancientolympic">
+      <Routes>
+        <Route path="/" index element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
