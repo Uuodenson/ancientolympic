9efb3eaaa991baad1d2de2920949fc3ee32b5de3
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2024-08-25 09:29:25 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import NotFound from "./router/NotFound.tsx";
 import "./index.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <HashRouter>
       <Routes>
         <Route path="/" index element={<App />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
   </StrictMode>
---
+++ src/router/NotFound.tsx
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <>
+      <div className="flex flex-col justify-center translate-y-1/2">
+        <h1 className="font-bold text-3xl">404</h1>
+        <p>Page not found</p>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
