299dea3d316a4de9dcc8e1fca855648cf44bca93
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2024-08-25 10:36:39 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import NotFound from "./router/NotFound.tsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/ancientolympic/">
+    <HashRouter basename="/ancientolympic/">
       <Routes>
         <Route index element={<App />} />
         <Route path="/404" element={<NotFound />}></Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
